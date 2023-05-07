74a38fcc99dc9da3a18ed3916911054fd5508f07
Orcino <94884406+0rcino@users.noreply.github.com>
2023-05-07 23:03:56 +0800

    C++ Square Area

---
+++ Square Area.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+  double Side; 
+  double Area; 
+
+  cout << "Enter the Area of Square: ";
+  cin >> Side; 
+
+  Area = Side*Side;
+
+  cout << "Area of the Square is: " << Area; 
+}
